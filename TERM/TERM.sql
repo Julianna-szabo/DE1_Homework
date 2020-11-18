a2f5e2357bfd9ceb2e76c991b56f99949cff85ed
Terez Julianna Szabo <szabo_terez@student.ceu.edu>
2020-11-18 08:59:53 +0100

    cleaning data and writing Trigger in script

--- TERM/TERM.sql
+++ TERM/TERM.sql
@@ -5,12 +5,18 @@ CREATE SCHEMA cruiseships;
 USE cruiseships;
 
 -- First I created and loaded all my data into the tables
+-- To do so, this first needs to be turned on
+
+SHOW VARIABLES LIKE "local_infile";
+SET GLOBAL local_infile = ON;
+SET FOREIGN_KEY_CHECKS=0;
+
 -- Creation of table #1
 
 DROP TABLE IF EXISTS ships;
 
 CREATE TABLE ships
-(name VARCHAR(50),
+(ship_name VARCHAR(50),
 callsign VARCHAR(50),
 wiki VARCHAR(100),
 imo VARCHAR(100),
@@ -26,73 +32,26 @@ shiplength INTEGER,
 vessel_type VARCHAR(100),
 flag VARCHAR(100),
 home_port VARCHAR(100), 
-PRIMARY KEY(name));
+PRIMARY KEY(ship_name));
 
 LOAD DATA LOCAL INFILE '/Users/Terez/Desktop/brandon-telle-cruise-ship-locations_copy/output_ships.csv'
 INTO TABLE ships
 FIELDS TERMINATED BY ','
 LINES TERMINATED BY '\n' 
 IGNORE 1 LINES
-(name,callsign,wiki,imo,mmsi,cruise_line,major_cruise_line,@year_built,class,gross_tonnage,decks,capacity,shiplength,vessel_type,flag,home_port)
+(ship_name,callsign,wiki,imo,mmsi,cruise_line,major_cruise_line,@year_built,class,gross_tonnage,decks,capacity,shiplength,vessel_type,flag,home_port)
 SET
 year_built = nullif(@year_built, '');
 
+SELECT * FROM ships;
+
 -- Creation of Table #2
-
-DROP TABLE IF EXISTS dailylocation;
-
-CREATE TABLE dailylocation
-(ship_name VARCHAR(50) NOT NULL,
-callsign VARCHAR(50),
-major_cruise_line VARCHAR(100),
-loc_date DATE,
-meters_traveled INTEGER,
-lat_start FLOAT,
-lon_start FLOAT,
-lat_end FLOAT,
-lon_end FLOAT,
-in_port VARCHAR(10),
-stopped_minutes INTEGER,
-num_periods INTEGER,
-port_city_id VARCHAR (100),
-port_city_name VARCHAR(100),
-port_city_country VARCHAR(100), 
-PRIMARY KEY(ship_name));
-
-LOAD DATA LOCAL INFILE '/Users/Terez/Desktop/brandon-telle-cruise-ship-locations_copy/output_daily_ship_location.csv'
-INTO TABLE dailylocation
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n' 
-IGNORE 1 LINES
-(ship_name,callsign,major_cruise_line,loc_date,meters_traveled,lat_start,lon_start,lat_end,lon_end,in_port,stopped_minutes,num_periods,port_city_id,port_city_name,port_city_country);
-
--- Creation of Table #3
-
-DROP TABLE IF EXISTS cities;
-
-CREATE TABLE cities
-(id VARCHAR(50),
-city VARCHAR(50),
-country VARCHAR (100),
-country_code VARCHAR(100),
-lat FLOAT,
-lon FLOAT,
-PRIMARY KEY(id));
-
-LOAD DATA LOCAL INFILE '/Users/Terez/Desktop/brandon-telle-cruise-ship-locations_copy/output_cities.csv'
-INTO TABLE cities
-FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n' 
-IGNORE 1 LINES
-(id,city,country,country_code,lat,lon);
-
--- Creation of Table #4 - MAYBE don't include since not relevant for analysis
 
 DROP TABLE IF EXISTS deaths;
 
 CREATE TABLE deaths
-(date_published DATE,
-death_time INTEGER,
+(ID INT NOT NULL AUTO_INCREMENT,
+date_published DATE,
 death_type VARCHAR(100),
 deceased_name VARCHAR(100),
 deceased_age INTEGER,
@@ -100,146 +59,249 @@ deceased_gender VARCHAR(10),
 is_passenger VARCHAR (10),
 is_crew VARCHAR(10),
 ship_cruise_line VARCHAR(50),
-ship_name VARCHAR(50),
+ship_name VARCHAR(50) NOT NULL,
 ship_callsign VARCHAR (50),
 url VARCHAR(100), 
-PRIMARY KEY(date_published, deceased_name));
+PRIMARY KEY(ID),
+FOREIGN KEY (ship_name)
+REFERENCES ships(ship_name));
 
-LOAD DATA LOCAL INFILE '/Users/Terez/Desktop/brandon-telle-cruise-ship-locations_copy/output_deaths.csv'
+LOAD DATA LOCAL INFILE '/Users/Terez/OneDrive - Central European University/Data_Engeniering_01/DE1_Homework/TERM/Cleaned_data/output_deaths.csv'
 INTO TABLE deaths
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\r\n' 
+LINES TERMINATED BY '\r' 
 IGNORE 1 LINES
-(date_published,death_time,death_type,deceased_name,deceased_age,deceased_gender,is_passenger,is_crew,ship_cruise_line,ship_name,ship_callsign,url);
+(date_published,death_type,deceased_name,@deceased_age,deceased_gender,is_passenger,is_crew,ship_cruise_line,ship_name,ship_callsign,url)
+SET
+	deceased_age = nullif(@deceased_age, '');
 
+SELECT * FROM deaths LIMIT 50;
 
-LOAD DATA LOCAL INFILE '/Users/Terez/Desktop/brandon-telle-cruise-ship-locations_copy/output_deaths.csv'
-INTO TABLE deaths
-CHARACTER SET latin1
+-- Creation of Table #3
+
+DROP TABLE IF EXISTS itinerary;
+
+CREATE TABLE itinerary
+(country VARCHAR (100),
+country_code VARCHAR (20),
+itinerary VARCHAR (100),
+PRIMARY KEY(country_code));
+
+LOAD DATA LOCAL INFILE '/Users/Terez/OneDrive - Central European University/Data_Engeniering_01/DE1_Homework/TERM/Cleaned_data/cruise_itinerary_data.csv'
+INTO TABLE itinerary
 FIELDS TERMINATED BY ','
-LINES TERMINATED BY '\n'
+LINES TERMINATED BY '\r' 
 IGNORE 1 LINES
-(date_published,death_time,death_type,deceased_name,deceased_age,deceased_gender,is_passenger,is_crew,ship_cruise_line,ship_name,ship_callsign,url);
+(country,country_code,itinerary);
 
-SELECT * FROM deaths;
+SELECT * FROM itinerary LIMIT 50;
+
+-- Creation of Table #4
+
+DROP TABLE IF EXISTS dailylocation;
+
+CREATE TABLE dailylocation
+(ID INT NOT NULL AUTO_INCREMENT,
+ship_name VARCHAR(50) NOT NULL,
+callsign VARCHAR(50),
+loc_date DATE,
+lat_start FLOAT,
+lon_start FLOAT,
+lat_end FLOAT,
+lon_end FLOAT,
+in_port VARCHAR(10),
+port_city_id VARCHAR (100),
+port_city_name VARCHAR(100),
+port_country VARCHAR (20),
+PRIMARY KEY(ID),
+FOREIGN KEY (ship_name)
+REFERENCES ships(ship_name),
+FOREIGN KEY (port_country)
+REFERENCES itinerary(country_code));
+
+LOAD DATA LOCAL INFILE '/Users/Terez/OneDrive - Central European University/Data_Engeniering_01/DE1_Homework/TERM/Cleaned_data/output_daily_ship_location.csv'
+INTO TABLE dailylocation
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES
+(ship_name,callsign,loc_date,lat_start,lon_start,lat_end,lon_end,in_port,port_city_id,port_city_name, port_country);
+
+SELECT * FROM dailylocation LIMIT 50;
+
 
 -- ANALYSIS
--- 1, How many large ships are in a certain city at once?
--- 2, How many ships from one company are docked in the same country on the same day?
--- 3, Where are the different ships of one fleet docked at the same time?
--- 4, Which ship did most people die on?
--- 5, Which route did most people die on? (use country to determine possible route)
+-- Which route did most people die on? (use country to determine possible route)
 
--- Analytical layer (Data warehouse) : All the ships in any port every day
+-- Analytical layer (Data warehouse) : All deaths on ships by date
 -- ETL: pulling the new daily entries to dailylocation into the Data Warehouse and from there to the Data Marks
--- Data marks: Different views by cruise line company, different views by port
+-- Data marks: Deaths comparing cruise lines, years, and routes
 
--- To be able to do the joins I need to delete some columns for the dailylocation table
+-- Now I can combine the tables with a JOIN to give me a new table to create an Analytical Data Layer
 
-ALTER TABLE dailylocation
-DROP COLUMN port_city_country;
-ALTER TABLE dailylocation
-DROP COLUMN major_cruise_line;
+-- First I am joining some of the smaller tables to make the future joins easer
 
--- Now I can combine the tables with a JOIN to give me a new table
+-- Deaths and ships
 
+DROP TABLE IF EXISTS deaths_on_ships;
+
+CREATE TABLE deaths_on_ships AS
+	SELECT
+		d.ID,
+        d.date_published,
+        d.is_passenger,
+        s.ship_name,
+        s.cruise_line,
+        s.major_cruise_line
+			FROM deaths AS d
+            LEFT JOIN ships AS s
+            USING (ship_name);
+
+SELECT * FROM deaths_on_ships;
+    
 -- Analytical layer table
 
-DROP TABLE IF EXISTS dailylocation_complete;
+DROP TABLE IF EXISTS deaths_by_itinerary;
 
-CREATE TABLE dailylocation_complete(
+CREATE TABLE deaths_by_itinerary(
 	ID INT NOT NULL AUTO_INCREMENT,
-	ship_name VARCHAR (50),
-    callsign VARCHAR (50),
-    cruise_line VARCHAR (50),
+	date_published DATE,
+    is_passenger VARCHAR (20),
+    ship_name VARCHAR (50),
     major_cruise_line VARCHAR (50),
-    loc_date DATE,
-    meters_traveled INT,
-    lat_start FLOAT,
-    lon_start FLOAT,
-    lat_end FLOAT,
-    lon_end FLOAT,
-    in_port VARCHAR (10),
-    stopped_minutes INT,
-    num_periods INT,
-    id_city VARCHAR (50),
-    city VARCHAR (50),
-    country VARCHAR (50),
-    country_code VARCHAR (10),
-    lat_city FLOAT,
-    lon_city FLOAT,
-    imo VARCHAR (100),
-    mmsi VARCHAR (100),
-    year_built INT,
-    class VARCHAR (50),
-    gross_tonnage INT,
-    decks INT,
-    capacity INT,
-    shiplength INT,
-    vessel_type VARCHAR (50),
-    flag VARCHAR (50),
-    home_port VARCHAR (100), 
+    itinerary VARCHAR (100),
     PRIMARY KEY(ID));
-
 
 -- ETL
 
 -- First importing of data
+-- For transformation, I 
+
+TRUNCATE deaths_by_itinerary;
 
 DROP PROCEDURE IF EXISTS aggregation;
 
 DELIMITER $$
 
 CREATE PROCEDURE aggregation()
-
 BEGIN
-	INSERT INTO dailylocation_complete
+	INSERT INTO deaths_by_itinerary
 	SELECT
 		NULL,
+		d.date_published,
+		d.is_passenger,
 		d.ship_name,
-		d.callsign,
-		s.cruise_line,
-		s.major_cruise_line,
-		d.loc_date,
-		d.meters_traveled,
-		d.lat_start,
-		d.lon_start,
-		d.lat_end,
-		d.lon_end,
-		d.in_port,
-		d.stopped_minutes,
-		d.num_periods,
-		c.id AS id_city,
-		c.city,
-		c.country,
-		c.country_code,
-		c.lat AS lat_city,
-		c.lon AS lon_city,
-		s.imo,
-		s.mmsi,
-		s.year_built,
-        s.class,
-		s.gross_tonnage,
-		s.decks,
-		s.capacity,
-		s.shiplength,
-		s.vessel_type,
-		s.flag,
-		s.home_port
-			FROM dailylocation d
-				LEFT JOIN cities c
-				ON port_city_id = id
-				LEFT JOIN ships s
-				USING (callsign);
+		d.major_cruise_line,
+		i.itinerary
+			FROM dailylocation dl
+				INNER JOIN deaths_on_ships d
+				ON dl.ship_name = d.ship_name
+					AND dl.loc_date = d.date_published
+				LEFT JOIN itinerary i
+				ON LEFT(dl.port_country,2) = i.country_code
+			ORDER BY d.date_published;
 END $$
 
 DELIMITER ;
 
 CALL aggregation();
 
-SELECT * FROM dailylocation_complete;
+SELECT * FROM deaths_by_itinerary LIMIT 10;
+
 
 -- Adding new data to the table when it is added in the operational layer
 
+DROP TABLE IF EXISTS messages;
 
+CREATE TABLE IF NOT EXISTS messages (
+message varchar(100) NOT NULL);
 
+DROP TRIGGER IF EXISTS deaths_insert;
+
+DELIMITER $$
+
+CREATE TRIGGER deaths_insert
+AFTER INSERT
+ON deaths FOR EACH ROW
+BEGIN
+	INSERT INTO messages SELECT CONCAT('death_ID: ', NEW.ID);
+    
+    INSERT INTO deaths_on_ships
+	SELECT
+		d.ID,
+        d.date_published,
+        d.is_passenger,
+        s.ship_name,
+        s.cruise_line,
+        s.major_cruise_line
+			FROM deaths AS d
+				LEFT JOIN ships AS s
+				USING (ship_name)
+            WHERE ID = NEW.ID;
+END $$    
+
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS deaths_insert_from_aggrgation; 
+
+DELIMITER $$
+
+CREATE TRIGGER deaths_insert_from_aggrgation
+AFTER INSERT
+ON deaths_on_ships FOR EACH ROW
+BEGIN
+	INSERT INTO messages SELECT CONCAT('death_on_ships_ID: ', NEW.ID);
+    
+    INSERT INTO deaths_by_itinerary
+	SELECT
+		NULL,
+		d.date_published,
+		d.is_passenger,
+		d.ship_name,
+		d.major_cruise_line,
+		i.itinerary
+			FROM dailylocation dl
+				INNER JOIN deaths_on_ships d
+				ON dl.ship_name = d.ship_name
+					AND dl.loc_date = d.date_published
+				LEFT JOIN itinerary i
+				ON LEFT(dl.port_country,2) = i.country_code
+			WHERE ID = NEW.ID
+            ORDER BY d.date_published;
+END $$
+
+DELIMITER ;
+
+INSERT INTO deaths
+VALUES(NULL,'2017-08-15', 'suicide', 'Someone John', 45, 'Male', 'TRUE', 'FALSE', 'Carnival Cruise Lines', 'Carnival Splender', '', 'www.tlc.com');
+SELECT * FROM deaths;
+
+-- Data Marks in the form of views
+
+-- Answer to original question
+-- Do more people die in the Carribbean or in Northern Europe?
+
+DROP VIEW IF EXISTS Northern_Europe_VS_Carribbean;
+
+CREATE VIEW `Northern_Europe_VS_Carribbean` AS
+SELECT * FROM deaths_by_itinerary WHERE deaths_by_itinerary.itinerary = 'Northern Europe' OR deaths_by_itinerary.itinerary = 'Carribbean and Antilles' ;
+
+SELECT itinerary,COUNT(*) FROM Northern_Europe_VS_Carribbean GROUP BY itinerary;
+
+-- Other interesting aspects
+-- How many passangers die a year?
+
+DROP VIEW IF EXISTS Passenger_deaths;
+
+CREATE VIEW `Passenger_deaths` AS
+SELECT is_passenger,date_published FROM deaths_by_itinerary WHERE deaths_by_itinerary.is_passenger = 'TRUE';
+
+SELECT YEAR(date_published), COUNT(*) FROM Passenger_deaths GROUP BY YEAR(date_published);
+
+-- Does Norvegian or Carneval have more total deaths?
+
+DROP VIEW IF EXISTS Norwegian_VS_Carnival;
+
+CREATE VIEW `Norwegian_VS_Carnival` AS
+SELECT date_published, major_cruise_line FROM deaths_by_itinerary WHERE major_cruise_line ='Norwegian Cruise Line' OR major_cruise_line ='Carnival Cruise Line';
+
+SELECT major_cruise_line, COUNT(*) FROM Norwegian_VS_Carnival GROUP BY major_cruise_line;
